d26aad9ca51a4efd7e29e95eb4b3c245ab3092d5
Jonatas Almeida <jonatas-almeida@users.noreply.github.com>
2020-11-08 18:17:40 -0300

    Filtro de Busca
    Foi adicionado filtro de busca para  o campo de buscar Eventos

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
+  _filtroLista: string;
+  //Encapsulamento da propriedade _filtroLista
+  get filtroLista(){
+    return this._filtroLista;
+  }
+  set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  eventosFiltrados: any = [];
   eventos: any = [];
+  //Opções definidas para serem settadas nos property binding das imagens no componente de eventos
   imagemLargura = 50;
   imagemMargem = 2;
   imagemBorda = 5;
@@ -21,6 +33,15 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  //Função para filtrar os eventos quando digitado no input de busca
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  //Mostra e esconde as imagens
   alternarImagem(){
     this.mostrarImagem = !this.mostrarImagem;
   }
