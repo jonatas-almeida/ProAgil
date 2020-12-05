9400d3960894cdc66f6ce3b56f8058f47255baec
Jonatas Almeida <jonatas-almeida@users.noreply.github.com>
2020-12-05 15:54:45 -0300

    Configurações Gerais e do JWT
    Foram adicionadas configurações gerais de autenticação e a configuração do JWT (Json Web Token)

--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -17,6 +17,13 @@ using AutoMapper;
 using Microsoft.Extensions.FileProviders;
 using System.IO;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using ProAgil.Domain.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace ProAgil.WebAPI
 {
@@ -32,9 +39,52 @@ namespace ProAgil.WebAPI
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddDbContext<ProAgilContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+      services.AddDbContext<ProAgilContext>(
+        x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
+      );
+
+      //Configuração geral
+      IdentityBuilder builder = services.AddIdentityCore<User>(options => {
+        //SENHA
+        //Resetando valores padrões da validação de senha
+        options.Password.RequireDigit = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 4;
+      });
+      //Instância do IdentityBuilder criado anteriormente(builder.Services)
+      //Configurações do Context, das Roles e dos Usuários
+      builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);
+      builder.AddEntityFrameworkStores<ProAgilContext>();
+      builder.AddRoleValidator<RoleValidator<Role>>();
+      builder.AddRoleManager<RoleManager<Role>>();
+      builder.AddSignInManager<SignInManager<User>>();
+
+      //Configuração do JWT (Json Web Token)
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+        options.TokenValidationParameters = new TokenValidationParameters{
+          //Assinatura da chave do emissor da api
+          ValidateIssuerSigningKey = true,
+          //Descriptografa a chave(Token) que estiver definido em AppSettings
+          IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+          
+          ValidateAudience = false,
+          ValidateIssuer = false
+        };
+      });
+
+      //Determina qual Controller será chamada
+      services.AddMvc(options => {
+          //Toda vez que uma rota for chamada, ele vai requerir que o usuário esteja autenticado
+          //Em sequência irá usar o AuthorizeFilter para filtrar todas as chamadas que tiver
+          var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+          options.Filters.Add(new AuthorizeFilter(policy));
+
+        }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2).AddJsonOptions(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+        //O AddJsonOptions controla qualquer redundância que houver em relação ao retorno da serialização dos itens. Resolve qualquer looping infinito que houver entre a relação das Entidades
+
       services.AddScoped<IProAgilRepository, ProAgilRepository>();
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
       //Definindo que a aplicação irá trabalhar com o AutoMapper
       services.AddAutoMapper();
       services.AddCors();
